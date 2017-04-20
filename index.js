a281ecc8710b875775d9f298de8773533a1883e7
Jacob Aronoff <jacobaronoff45@gmail.com>
2017-04-20 00:55:14 -0400

    update to data stuff

--- index.js
+++ index.js
@@ -8,11 +8,18 @@ const projectId = 'redditcollaborativefiltering';
 
 // Instantiates a client
 const bigquery = BigQuery({
-    projectId: projectId
+  projectId: projectId
 });
 var query = 'SELECT * FROM [fh-bigquery:reddit_comments.2017_02] where author="ThatGuyWhoSucksAtLOL" LIMIT 10';
 
-
+bigquery.createQueryStream(query)
+  .on('error', console.error)
+  .on('data', function(row) {
+    console.log(row);
+  })
+  .on('end', function() {
+    // All rows retrieved.
+  });
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -23,21 +30,11 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-    response.render('pages/index');
+  response.render('pages/index');
 });
-
-app.get('/data', function(request, response) {
-    bigquery.createQueryStream(query)
-        .on('error', console.error)
-        .on('data', function(row) {
-            console.log(row);
-            return row;
-        })
-        .on('end', function() {
-            // All rows retrieved.
-        });
-})
 
 app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
+  console.log('Node app is running on port', app.get('port'));
 });
+
+
